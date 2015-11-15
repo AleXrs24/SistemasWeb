661de158dd1a706c9fb7ecfeceb37607b25f032f
xetxeberria <xabietxe93@gmail.com>
2015-11-15 19:10:36 +0100

    El ultimo intento

--- clientePass.php
+++ clientePass.php
@@ -3,7 +3,7 @@
 	require_once('lib/class.wsdlcache.php');
 
 	//$cliente = new nusoap_client("http://localhost/SistemasWeb/ComprobarContraseña.php?wsdl", false);
-	$cliente = new nusoap_client("http://http://swetxeberria.hol.es/ComprobarContrasena.php?wsdl", false);
+	$cliente = new nusoap_client("http://swetxeberria.hol.es/ComprobarContrasena.php?wsdl", false);
 
 	$result = $cliente->call('comprobarPass', array('x'=>$_GET['pass']));
 	
