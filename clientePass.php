42c7b92fc74b8772b43f2463d8934075c010b972
xetxeberria <xabietxe93@gmail.com>
2015-11-15 19:01:58 +0100

    Cambiado nombre de ComprobarContraseña.php a ComprbarContrasena.php

--- clientePass.php
+++ clientePass.php
@@ -3,7 +3,7 @@
 	require_once('lib/class.wsdlcache.php');
 
 	//$cliente = new nusoap_client("http://localhost/SistemasWeb/ComprobarContraseña.php?wsdl", false);
-	$cliente = new nusoap_client("http://http://swetxeberria.hol.esComprobarContraseña.php?wsdl", false);
+	$cliente = new nusoap_client("http://http://swetxeberria.hol.es/ComprobarContrasena.php?wsdl", false);
 
 	$result = $cliente->call('comprobarPass', array('x'=>$_GET['pass']));
 	
